af37f3d2f1f11c66fd4a93a5eb813c5e68ce99ae
Gabriele <gabriele.palma.bool@gmail.com>
2023-11-22 18:05:34 +0100

    create first if/else, to fix

--- Esercizio-mail/Javascript/Jv.js
+++ Esercizio-mail/Javascript/Jv.js
@@ -16,10 +16,8 @@ const delButton = document.getElementById("cencella");
 // acquisisco l'elemento che ha per classe risultato, dove andrò ad inserire il risultato
 const risultato = document.querySelector(".string");
 
-console.log (userMail);
 
-
-const mails = ["pippo@hotmail.com", "pluto@outlook.com"]
+const mails = ["pippo", "j", "hj", "gj"]
 
 // Quando viene cliccato il pulsante invia
 search.addEventListener("click",
@@ -27,6 +25,9 @@ search.addEventListener("click",
     function () {
 
         let val1 = userMail.value;
+
+        document.getElementById("none").style="display:block"; 
+
         Fsearch(val1);
     }
 );
@@ -48,27 +49,61 @@ function Fsearch(val1) {
 
     let mailU = val1;
 
-    console.log (mailU);
+    
 
-    for (let i = 0; i<=mails.length; i++) {
+    for (let i = 0; i < mails.length; i++) {
             
-        let xMail = mails[i];
+        // inizializzo una variabile ("contatore") a zero
+        let num = 0;
         
-        if (mailU == xMail){
+        //acquisisco il valore ("stringa") nella prima posizione della mia stringa
+        let xMail= mails[i];
+       
+        console.log (mailU);
+        console.log (xMail);
 
-            document.getElementById("none").style="display:block"; 
+        //confronto i valori e in caso risultino diversi
+        if (mailU != xMail){
 
-            //Creo l'elemento all'interno del mio file html
-            const stringa = document.createElement ("p");
+           //aggiorno il valore ogni volta che controllo il valore e risulta diverso 
+           num = i +1;
 
-            //inserisco scritta all'interno del mio contenuto
-            stringa.append("Sei dei nostri, puoi procedere!");
+           //Condizione necessaria per inserire l'elemento all'interno del mio array (se anche l'ultimo elemento della mia lista è diverso e sono sicuro di aver confrontato ogni altro elemento prima)
+            if (num == mails.length){
+                
+                //Creo l'elemento all'interno del mio file html
+                const stringa = document.createElement ("p");
 
-            //inserisco stringa all'interno del mio contenitore "none"
-            risultato.append(stringa);
+                //inserisco scritta all'interno del mio contenuto
+                stringa.append("la mail non è presente nell'archivio, procedo all'inserimento!.......riprovare");
+
+                //inserisco stringa all'interno del mio contenitore "none"
+                risultato.append(stringa);
+            
+                // Inserisco l'elemento in fondo al mio array 
+                mails.push(mailU);
+
+                console.log (mails);
+                
+              }
         }
 
+        else {
 
+            //Creo l'elemento all'interno del mio file html
+             const stringa = document.createElement ("p");
+
+             //inserisco scritta all'interno del mio contenuto
+             stringa.append("Sei dei nostri, puoi procedere!");
+
+             //inserisco stringa all'interno del mio contenitore "none"
+             risultato.append(stringa);
+            
+        }
+
+        
+
+       
     }
 
 
